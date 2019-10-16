74f5eb9f2ae0aeae9fecd807130e7fde72c9be3e
Yoav Hayun <yoavhayun@gmail.com>
2019-10-16 18:00:36 +0300

    Organize unittests layout

--- CLI_Unittest.py
+++ CLI_Unittest.py
@@ -2,6 +2,16 @@ import unittest, traceback
 from class_cli.cli import CLI
 import class_cli._cli_exception as cli_exception
 
+
+
+
+
+#############
+#           #
+#  General  #
+#           #
+#############
+
 class General(unittest.TestCase):
     def test_help_behavior(self):
         """
@@ -13,6 +23,16 @@ class General(unittest.TestCase):
             pass
 
         Tester().CLI.run("--help")
+
+
+
+
+
+#################
+#               #
+#  Compilation  #
+#               #
+#################
 
 class Compilation(unittest.TestCase):
 
@@ -245,217 +265,11 @@ class Compilation(unittest.TestCase):
 
 
 
-class Settings(unittest.TestCase):
-
-    def setUp(self):
-        cli = CLI()
-
-        values = {"int":0, "str":'', "dict":{}, "list":[]}
-
-        @cli.Program(verbosity=None)
-        class Tester: 
-
-            def __init__(self):
-                self._silenced = False
-
-            @cli.Setting(initial_value='')
-            def set_str(self, value):
-                return value
-
-            @cli.Setting(initial_value=0)
-            def set_int(self, value:int):
-                return value
-
-            @cli.Setting(initial_value=tuple())
-            def set_args(self, *values):
-                return values
-
-            @cli.Setting(initial_value={})
-            def set_kwargs(self, **values):
-                return values
-
-            @cli.Setting(initial_value=None)
-            def set_dict(self, value:values):
-                return value
-
-            @cli.Setting(initial_value=None)
-            def set_iterable(self, value:values.keys()):
-                return value
-
-            @cli.Setting(initial_value=None, updates_value=False)
-            def set_constant(self, value):
-                return value
-
-            @cli.Validation()
-            def set_exception(self, value):
-                if not self._silenced:
-                    raise Exception()
-
-            @cli.Setting(initial_value=None)
-            def set_exception(self, value):
-                return value
-
-            def _silence_exception(self):
-                self._silenced = True
-
-        self.tester = Tester
-        self.values = values
-
-
-    ERROR_INITIAL_VALUE = 'Setting initial value was not set correctly'
-    ERROR_INSTANCE_CALL = 'Calling setting method directly from instance did not return the expected value.'
-    ERROR_CLI_CALL = 'Calling setting method from CLI did not return the expected value.'
-    ERROR_SETTING_NOT_MODIFIED = 'Setting value was not modified as expected.'
-    ERROR_SETTING_MODIFIED = 'Setting value was modified, although it was not expected to.'
-
-    def test_setting_str(self):
-        """
-        Test setting of type 'str'
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_str, '', Settings.ERROR_INITIAL_VALUE)
-
-        expected = '1'
-        self.assertEqual(tester.set_str("1"), expected, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_str, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-        expected = '2'
-        self.assertEqual(tester.CLI.execute(".setting", "set_str", "2"), expected, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_str, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_setting_int(self):
-        """
-        Test setting of type 'int'
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_int, 0, Settings.ERROR_INITIAL_VALUE)
-
-        expected = 1
-        self.assertEqual(tester.set_int(1), expected, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_int, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-        expected = 2
-        self.assertEqual(tester.CLI.execute(".setting", "set_int", "2"), expected, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_int, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_setting_args(self):
-        """
-        Test setting that accepts *args
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_args, tuple(), Settings.ERROR_INITIAL_VALUE)
-
-        expected = (1, 2, 3, 4)
-        self.assertEqual(tester.set_args(1, 2, 3, 4), expected, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_args, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-        expected = ('1', '2', '3', '4')
-        self.assertEqual(tester.CLI.execute(".setting", "set_args", "1", "2", "3", "4"), expected, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_args, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_setting_kwargs(self):
-        """
-        Test setting that accepts *kwargs
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_kwargs, {}, Settings.ERROR_INITIAL_VALUE)
-
-        expected = {"_1":1, "_2":2, "_3":3, "_4":4}
-        self.assertEqual(tester.set_kwargs(_1=1, _2=2, _3=3, _4=4), expected, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_kwargs, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-        expected = {"1":"1", "2":"2", "3":"3", "4":"4"}
-        self.assertEqual(tester.CLI.execute(".setting", "set_kwargs", "1=1", "2=2", "3=3", "4=4"), expected, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_kwargs, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_setting_dict(self):
-        """
-        Test setting that accepts values from a dict
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_dict, None, Settings.ERROR_INITIAL_VALUE)
-
-        self.values["new"] = "New Value"
-        for key in self.values:
-            expected = self.values[key]
-            self.assertEqual(tester.CLI.execute(".setting", "set_dict", key), expected, Settings.ERROR_CLI_CALL)
-            self.assertEqual(tester.CLI.set_dict, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_setting_iterable(self):
-        """
-        Test setting that accepts values from an iterable
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_iterable, None, Settings.ERROR_INITIAL_VALUE)
-
-        self.values["new"] = "New Value"
-        for key in self.values:
-            expected = key
-            self.assertEqual(tester.CLI.execute(".setting", "set_iterable", key), expected, Settings.ERROR_CLI_CALL)
-            self.assertEqual(tester.CLI.set_iterable, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-        non_existing = '-'
-
-        with self.assertRaises(cli_exception.InputException):
-            tester.CLI.execute(".setting", "set_iterable", non_existing)
-
-    def test_setting_constant(self):
-        """
-        Test setting that has a constant value
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_constant, None, Settings.ERROR_INITIAL_VALUE)
-
-        expected_return = ''
-        expected_value = None
-        self.assertEqual(tester.set_constant(''), expected_return, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_constant, expected_value, Settings.ERROR_SETTING_MODIFIED)
-
-        self.assertEqual(tester.CLI.execute(".setting", "set_constant" ,''), expected_return, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_constant, expected_value, Settings.ERROR_SETTING_MODIFIED)
-
-
-    def test_setting_exception(self):
-        """
-        Test setting that has Validation() attached
-        """
-        tester = self.tester()
-        self.assertEqual(tester.CLI.set_exception, None, Settings.ERROR_INITIAL_VALUE)
-
-        with self.assertRaises(Exception):
-            tester.set_exception('0')
-        self.assertEqual(tester.CLI.set_exception, None, Settings.ERROR_SETTING_MODIFIED)
-
-        expected = None
-        self.assertEqual(tester.CLI.execute(".setting", "set_exception", "0"), expected)
-        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_MODIFIED)
-        
-        tester._silence_exception()
-        expected = ''
-        self.assertEqual(tester.set_exception(''), expected, Settings.ERROR_INSTANCE_CALL)
-        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-        expected = '0'
-        self.assertEqual(tester.CLI.execute(".setting", "set_exception", "0"), expected, Settings.ERROR_CLI_CALL)
-        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
-
-    def test_assert_setting_isolation(self):
-        """
-        Test that 2 different instances operate on different settings
-        """
-        tester1 = self.tester()
-        tester2 = self.tester()
-        self.assertEqual(tester1.CLI.set_str, tester2.CLI.set_str, "Setup failed to produce the same setting value for 2 different instances")
-        tester1.set_str("value")
-        self.assertEqual(tester1.CLI.set_str, "value", Settings.ERROR_SETTING_NOT_MODIFIED)
-        self.assertNotEqual(tester1.CLI.set_str, tester2.CLI.set_str, Settings.ERROR_SETTING_MODIFIED)
-        tester2.set_str("value")
-        self.assertEqual(tester1.CLI.set_str, tester2.CLI.set_str, Settings.ERROR_SETTING_MODIFIED)
-
-
-
-
-
-
-
-
+#############
+#           #
+#  Methods  #
+#           #
+#############
 
 class Methods(unittest.TestCase):
 
@@ -731,6 +545,182 @@ class Parser(unittest.TestCase):
         with self.assertRaises(cli_exception.InputException):
             tester.CLI.run("oper_all - a=1 a b b=2 c c=3")
 
+
+
+
+
+##############
+#            #
+#  Settings  #
+#            #
+##############
+
+class Settings(unittest.TestCase):
+
+    def setUp(self):
+        cli = CLI()
+
+        values = {"int":0, "str":'', "dict":{}, "list":[]}
+
+        @cli.Program(verbosity=None)
+        class Tester: 
+
+            def __init__(self):
+                self._silenced = False
+
+            @cli.Setting(initial_value='')
+            def set_str(self, value):
+                return value
+
+            @cli.Setting(initial_value=0)
+            def set_int(self, value:int):
+                return value
+
+            @cli.Setting(initial_value=None)
+            def set_dict(self, value:values):
+                return value
+
+            @cli.Setting(initial_value=None)
+            def set_iterable(self, value:values.keys()):
+                return value
+
+            @cli.Setting(initial_value=None, updates_value=False)
+            def set_constant(self, value):
+                return value
+
+            @cli.Validation()
+            def set_exception(self, value):
+                if not self._silenced:
+                    raise Exception()
+
+            @cli.Setting(initial_value=None)
+            def set_exception(self, value):
+                return value
+
+            def _silence_exception(self):
+                self._silenced = True
+
+        self.tester = Tester
+        self.values = values
+
+
+    ERROR_INITIAL_VALUE = 'Setting initial value was not set correctly'
+    ERROR_INSTANCE_CALL = 'Calling setting method directly from instance did not return the expected value.'
+    ERROR_CLI_CALL = 'Calling setting method from CLI did not return the expected value.'
+    ERROR_SETTING_NOT_MODIFIED = 'Setting value was not modified as expected.'
+    ERROR_SETTING_MODIFIED = 'Setting value was modified, although it was not expected to.'
+
+    def test_setting_str(self):
+        """
+        Test setting of type 'str'
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_str, '', Settings.ERROR_INITIAL_VALUE)
+
+        expected = '1'
+        self.assertEqual(tester.set_str("1"), expected, Settings.ERROR_INSTANCE_CALL)
+        self.assertEqual(tester.CLI.set_str, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+        expected = '2'
+        self.assertEqual(tester.CLI.execute(".setting", "set_str", "2"), expected, Settings.ERROR_CLI_CALL)
+        self.assertEqual(tester.CLI.set_str, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+    def test_setting_int(self):
+        """
+        Test setting of type 'int'
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_int, 0, Settings.ERROR_INITIAL_VALUE)
+
+        expected = 1
+        self.assertEqual(tester.set_int(1), expected, Settings.ERROR_INSTANCE_CALL)
+        self.assertEqual(tester.CLI.set_int, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+        expected = 2
+        self.assertEqual(tester.CLI.execute(".setting", "set_int", "2"), expected, Settings.ERROR_CLI_CALL)
+        self.assertEqual(tester.CLI.set_int, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+    def test_setting_dict(self):
+        """
+        Test setting that accepts values from a dict
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_dict, None, Settings.ERROR_INITIAL_VALUE)
+
+        self.values["new"] = "New Value"
+        for key in self.values:
+            expected = self.values[key]
+            self.assertEqual(tester.CLI.execute(".setting", "set_dict", key), expected, Settings.ERROR_CLI_CALL)
+            self.assertEqual(tester.CLI.set_dict, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+    def test_setting_iterable(self):
+        """
+        Test setting that accepts values from an iterable
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_iterable, None, Settings.ERROR_INITIAL_VALUE)
+
+        self.values["new"] = "New Value"
+        for key in self.values:
+            expected = key
+            self.assertEqual(tester.CLI.execute(".setting", "set_iterable", key), expected, Settings.ERROR_CLI_CALL)
+            self.assertEqual(tester.CLI.set_iterable, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+        non_existing = '-'
+
+        with self.assertRaises(cli_exception.InputException):
+            tester.CLI.execute(".setting", "set_iterable", non_existing)
+
+    def test_setting_constant(self):
+        """
+        Test setting that has a constant value
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_constant, None, Settings.ERROR_INITIAL_VALUE)
+
+        expected_return = ''
+        expected_value = None
+        self.assertEqual(tester.set_constant(''), expected_return, Settings.ERROR_INSTANCE_CALL)
+        self.assertEqual(tester.CLI.set_constant, expected_value, Settings.ERROR_SETTING_MODIFIED)
+
+        self.assertEqual(tester.CLI.execute(".setting", "set_constant" ,''), expected_return, Settings.ERROR_CLI_CALL)
+        self.assertEqual(tester.CLI.set_constant, expected_value, Settings.ERROR_SETTING_MODIFIED)
+
+
+    def test_setting_exception(self):
+        """
+        Test setting that has Validation() attached
+        """
+        tester = self.tester()
+        self.assertEqual(tester.CLI.set_exception, None, Settings.ERROR_INITIAL_VALUE)
+
+        with self.assertRaises(Exception):
+            tester.set_exception('0')
+        self.assertEqual(tester.CLI.set_exception, None, Settings.ERROR_SETTING_MODIFIED)
+
+        expected = None
+        self.assertEqual(tester.CLI.execute(".setting", "set_exception", "0"), expected)
+        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_MODIFIED)
+        
+        tester._silence_exception()
+        expected = ''
+        self.assertEqual(tester.set_exception(''), expected, Settings.ERROR_INSTANCE_CALL)
+        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+        expected = '0'
+        self.assertEqual(tester.CLI.execute(".setting", "set_exception", "0"), expected, Settings.ERROR_CLI_CALL)
+        self.assertEqual(tester.CLI.set_exception, expected, Settings.ERROR_SETTING_NOT_MODIFIED)
+
+    def test_assert_setting_isolation(self):
+        """
+        Test that 2 different instances operate on different settings
+        """
+        tester1 = self.tester()
+        tester2 = self.tester()
+        self.assertEqual(tester1.CLI.set_str, tester2.CLI.set_str, "Setup failed to produce the same setting value for 2 different instances")
+        tester1.set_str("value")
+        self.assertEqual(tester1.CLI.set_str, "value", Settings.ERROR_SETTING_NOT_MODIFIED)
+        self.assertNotEqual(tester1.CLI.set_str, tester2.CLI.set_str, Settings.ERROR_SETTING_MODIFIED)
+        tester2.set_str("value")
+        self.assertEqual(tester1.CLI.set_str, tester2.CLI.set_str, Settings.ERROR_SETTING_MODIFIED)
 
 
 if __name__ == "__main__":
