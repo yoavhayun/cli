be0793b42f079bf8bd22b0ed78b8029bfbb78519
Yoav Hayun <yoavhayun@gmail.com>
2019-10-16 15:38:23 +0300

    Adding Compilation Tests + Cleanup

--- CLI_Unittest.py
+++ CLI_Unittest.py
@@ -4,6 +4,9 @@ import class_cli._cli_exception as cli_exception
 
 class TestCLI__General(unittest.TestCase):
     def test_help_behavior(self):
+        """
+        Test that calling help does not raise an Exception from argparse
+        """
         cli = CLI()
         @cli.Program()
         class Tester:
@@ -14,7 +17,9 @@ class TestCLI__General(unittest.TestCase):
 class TestCLI_Compilation(unittest.TestCase):
 
     def test_missing_implementation(self):
-
+        """
+        Test for exception when there is Validation without a Setting or an Operation
+        """
         cli = CLI()
         @cli.Program(verbosity=None)
         class Tester:
@@ -41,6 +46,9 @@ class TestCLI_Compilation(unittest.TestCase):
         Tester()
     
     def test_duplicate_names(self):
+        """
+        Test that a method cannot have multiple executions for the same name
+        """
         cli = CLI()
 
         with self.assertRaises(cli_exception.CompilationException):
@@ -51,8 +59,27 @@ class TestCLI_Compilation(unittest.TestCase):
                 @cli.Setting()
                 def foo(self): pass
 
+        with self.assertRaises(cli_exception.CompilationException):
+            @cli.Program(verbosity=None)
+            class Tester:
+                @cli.Operation()
+                def foo(self): pass
+                @cli.Operation()
+                def foo(self): pass
+
+        with self.assertRaises(cli_exception.CompilationException):
+            @cli.Program(verbosity=None)
+            class Tester:
+                @cli.Setting()
+                def foo(self): pass
+                @cli.Setting()
+                def foo(self): pass
+
 
     def test_multiple_instances(self):
+        """
+        Tests isolation between multiple instances
+        """
         cli = CLI()
 
         @cli.Program(verbosity=None)
@@ -67,15 +94,120 @@ class TestCLI_Compilation(unittest.TestCase):
         self.assertNotEqual(tester1._value, tester2._value, TestCLI_Methods.ERROR_FIELD_MODIFIED)
         tester2._value = 1
         self.assertEqual(tester1._value, tester2._value, TestCLI_Methods.ERROR_FIELD_MODIFIED)
-
-    def test_non_matching_signature(self):
+    
+    def test_matching_signature(self):
+        """
+        Tests that matching signatures compiles
+        """
+        class TMP: pass
         cli = CLI()
-
         @cli.Program(verbosity=None)
         class Tester:
-            pass
+            @cli.Validation()
+            def oper(self, a, _a, b=1, c=[0, ''], *args, **kwargs): pass
+            @cli.Operation()
+            def oper(self, a, _a, b=1, c=[0, ''], *args, **kwargs): pass
+        Tester()
+
+    def test_non_matching_signature(self):
+        """
+        Tests for exception when defining non-matching method signature
+        """
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, a): pass
+            @cli.Operation()
+            def oper(self, b): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on different names"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, *a): pass
+            @cli.Operation()
+            def oper(self, *b): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on different *args names"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, **a): pass
+            @cli.Operation()
+            def oper(self, **b): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on different **kwargs names"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, *a): pass
+            @cli.Operation()
+            def oper(self): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on missing *args"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self): pass
+            @cli.Operation()
+            def oper(self, *a): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on missing *args"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, **a): pass
+            @cli.Operation()
+            def oper(self): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on missing **kwargs"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self): pass
+            @cli.Operation()
+            def oper(self, **a): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on missing **kwargs"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, a=0): pass
+            @cli.Operation()
+            def oper(self, a): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on non matching default values"): 
+            Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            @cli.Validation()
+            def oper(self, a): pass
+            @cli.Operation()
+            def oper(self, a=0): pass
+        with self.assertRaises(cli_exception.InitializationException, "Failed on non matching default values"): 
+            Tester()
 
     def test_constructor_accessability(self):
+        """
+        Tests for exception when trying to access CLI methods or setting before
+        """
         cli = CLI()
         @cli.Program(verbosity=None)
         class Tester:
@@ -87,6 +219,22 @@ class TestCLI_Compilation(unittest.TestCase):
                 return value
 
         with self.assertRaises(cli_exception.InitializationException): Tester()
+
+        cli = CLI()
+        @cli.Program(verbosity=None)
+        class Tester:
+            def __init__(self):
+                self.CLI.value
+            
+            @cli.Setting()
+            def value(self, values):
+                return value
+
+        with self.assertRaises(AttributeError): Tester()
+
+
+
+
 
 class TestCLI_Settings(unittest.TestCase):
 
@@ -234,6 +382,11 @@ class TestCLI_Settings(unittest.TestCase):
             self.assertEqual(tester.CLI.execute(".setting", "set_iterable", key), expected, TestCLI_Settings.ERROR_CLI_CALL)
             self.assertEqual(tester.CLI.set_iterable, expected, TestCLI_Settings.ERROR_SETTING_NOT_MODIFIED)
 
+        non_existing = '-'
+
+        with self.assertRaises(cli_exception.InputException):
+            tester.CLI.execute(".setting", "set_iterable", non_existing)
+
     def test_setting_constant(self):
         """
         Test setting that has a constant value
@@ -275,6 +428,9 @@ class TestCLI_Settings(unittest.TestCase):
         self.assertEqual(tester.CLI.set_exception, expected, TestCLI_Settings.ERROR_SETTING_NOT_MODIFIED)
 
     def test_assert_setting_isolation(self):
+        """
+        Test that 2 different instances operate on different settings
+        """
         tester1 = self.tester()
         tester2 = self.tester()
         self.assertEqual(tester1.CLI.set_str, tester2.CLI.set_str, "Setup failed to produce the same setting value for 2 different instances")
@@ -441,6 +597,11 @@ class TestCLI_Methods(unittest.TestCase):
             expected = key
             self.assertEqual(tester.CLI.execute("op_iterable", key), expected, TestCLI_Methods.ERROR_CLI_CALL)
             self.assertEqual(tester._value, expected, TestCLI_Methods.ERROR_FIELD_NOT_MODIFIED)
+        
+        non_existing = '-'
+
+        with self.assertRaises(cli_exception.InputException):
+            tester.CLI.execute("op_iterable", non_existing)
 
 
     def test_operation_exception(self):
@@ -468,6 +629,9 @@ class TestCLI_Methods(unittest.TestCase):
         self.assertEqual(tester._value, expected, TestCLI_Methods.ERROR_FIELD_NOT_MODIFIED)
 
     def test_assert_operation_isolation(self):
+        """
+        Test that 2 different instances operate on different methods
+        """
         tester1 = self.tester()
         tester2 = self.tester()
         self.assertEqual(tester1._value, tester2._value, "Setup failed to produce the same field value for 2 different instances")
@@ -508,6 +672,9 @@ class TestCLI_Parser(unittest.TestCase):
         self.tester = Tester
 
     def test_parser_type(self):
+        """
+        Test for parser type convertions
+        """
         tester = self.tester()
         self.assertEqual(tester.CLI.run("oper_type 5"), tester.oper_type(5))
         self.assertEqual(tester.CLI.run("oper_type -5"), tester.oper_type(-5))
@@ -515,12 +682,18 @@ class TestCLI_Parser(unittest.TestCase):
             tester.CLI.run("oper_type -")
 
     def test_parser_args(self):
+        """
+        Test for parsers handeling of *args
+        """
         tester = self.tester()
         self.assertEqual(tester.CLI.run("oper_args a"), tester.oper_args('a'))
         self.assertEqual(tester.CLI.run("  oper_args  a  "), tester.oper_args('a'))
         self.assertEqual(tester.CLI.run("oper_args a   b    c"), tester.oper_args('a', 'b', 'c'))
 
     def test_parser_kwargs(self):
+        """
+        Test for parsers handeling of **kwargs
+        """
         tester = self.tester()
         with self.assertRaises(cli_exception.InputException):
             tester.CLI.run("oper_kwargs a")
@@ -528,6 +701,9 @@ class TestCLI_Parser(unittest.TestCase):
         self.assertEqual(tester.CLI.run("oper_kwargs a=1   b=2    c=3"), tester.oper_kwargs(a='1', b='2', c='3'))
 
     def test_parser_both(self):
+        """
+        Test for parsers handeling of both *args and **kwargs
+        """
         tester = self.tester()
         self.assertEqual(tester.CLI.run("oper_both a"), tester.oper_both('a'))
         self.assertEqual(tester.CLI.run("oper_both a=1"), tester.oper_both(a='1'))
@@ -537,6 +713,9 @@ class TestCLI_Parser(unittest.TestCase):
         self.assertEqual(tester.CLI.run("oper_both a=1 a b b=2 c c=3"), tester.oper_both('a', 'b', 'c', a='1', b='2', c='3'))
 
     def test_parser_all(self):
+        """
+        Test for parsers handeling of both *args and **kwargs and an expected argument with type convertion
+        """
         tester = self.tester()
         self.assertEqual(tester.CLI.run("oper_all 5 a=1 a b b=2 c c=3"), tester.oper_all(5, 'a', 'b', 'c', a='1', b='2', c='3'))
         self.assertEqual(tester.CLI.run("oper_all 5 a=1 a b b=2 c c=3"), tester.oper_all(5, 'a', 'b', 'c', a='1', b='2', c='3'))
