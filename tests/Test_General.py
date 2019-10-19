a91321bd074506c499bb7fcbd2ddde8a9260d66b
Yoav Hayun <yoavhayun@gmail.com>
2019-10-19 17:30:59 +0300

    Add a test for CLI accessibility

--- tests/Test_General.py
+++ tests/Test_General.py
@@ -12,3 +12,28 @@ class General(unittest.TestCase):
             pass
 
         Tester().CLI.run("--help")
+
+    def test_cli_access(self):
+        """
+        Test that calling help does not raise an Exception from argparse
+        """
+        cli = CLI()
+        @cli.Program()
+        class Tester:
+            @cli.Setting()
+            def set(self, value): return value
+
+            @cli.Operation()
+            def get_set(self, value=None): 
+                if value is not None:
+                    self.set(value)
+                return self.CLI.set
+
+        tester = Tester()
+        self.assertEqual(tester.CLI.execute("get_set"), tester.CLI.set)
+        tester.set('1')
+        self.assertEqual(tester.CLI.set, '1')
+        self.assertEqual(tester.CLI.execute("get_set"), tester.CLI.set)
+        self.assertEqual(tester.CLI.execute("get_set", '2'), tester.CLI.set)
+        self.assertEqual(tester.CLI.set, tester.CLI.execute("get_set", '2'))
+        self.assertEqual(tester.CLI.set, '2')
