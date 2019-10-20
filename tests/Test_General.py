95542ee18dbc077909ae3c9b731a034cb30b5de2
Yoav Hayun <yoavhayun@gmail.com>
2019-10-20 09:45:23 +0300

    Better quote support

--- tests/Test_General.py
+++ tests/Test_General.py
@@ -13,9 +13,40 @@ class General(unittest.TestCase):
 
         Tester().CLI.run("--help")
 
+    def test_handling_quotes(self):
+        """
+        Test that inputs that contain quotes work as expected
+        """
+        cli = CLI()
+        @cli.Program()
+        class Tester:
+            @cli.Operation()
+            def oper(self, value): return value
+
+            @cli.Delegate()
+            def delegate(self): return self
+
+        tester = Tester()
+        self.assertEqual(tester.CLI.run_line("oper 'oneword'"), "oneword")
+        self.assertEqual(tester.CLI.run_line('oper "oneword"'), "oneword")
+        self.assertEqual(tester.CLI.run_line("oper 'two words'"), "two words")
+        self.assertEqual(tester.CLI.run_line('oper "two words"'), "two words")
+        self.assertEqual(tester.CLI.run_line("oper 'two words'"), "two words")
+        self.assertEqual(tester.CLI.run_line('oper "prefix \'inner words\' suffix"'), "prefix 'inner words' suffix")
+        self.assertEqual(tester.CLI.run_line("oper 'prefix \"inner words\" suffix'"), 'prefix "inner words" suffix')
+
+        self.assertEqual(tester.CLI.run_line("delegate oper 'oneword'"), "oneword")
+        self.assertEqual(tester.CLI.run_line('delegate oper "oneword"'), "oneword")
+        self.assertEqual(tester.CLI.run_line("delegate oper 'two words'"), "two words")
+        self.assertEqual(tester.CLI.run_line('delegate oper "two words"'), "two words")
+        self.assertEqual(tester.CLI.run_line('delegate oper "prefix \'inner words\' suffix"'), "prefix 'inner words' suffix")
+        self.assertEqual(tester.CLI.run_line("delegate oper 'prefix \"inner words\" suffix'"), 'prefix "inner words" suffix')
+
+        self.assertEqual(tester.CLI.run_line("delegate oper '\"nospace\"'"), '"nospace"')
+
     def test_cli_access(self):
         """
-        Test that calling help does not raise an Exception from argparse
+        Test that methods can access the CLI keyword and calls compiled methods.
         """
         cli = CLI()
         @cli.Program()
@@ -29,11 +60,16 @@ class General(unittest.TestCase):
                     self.set(value)
                 return self.CLI.set
 
+            @cli.Delegate()
+            def delegate(self): return self
+
         tester = Tester()
         self.assertEqual(tester.CLI.execute("get_set"), tester.CLI.set)
         tester.set('1')
         self.assertEqual(tester.CLI.set, '1')
         self.assertEqual(tester.CLI.execute("get_set"), tester.CLI.set)
         self.assertEqual(tester.CLI.execute("get_set", '2'), tester.CLI.set)
+        self.assertEqual(tester.CLI.execute("delegate", "get_set"), tester.CLI.set)
         self.assertEqual(tester.CLI.set, tester.CLI.execute("get_set", '2'))
         self.assertEqual(tester.CLI.set, '2')
+        self.assertEqual(tester.delegate().CLI.set, '2')
